68ba2667a554c2d0b0f53e22796b46eb8da96c42
Sivaraj Kumar N <sivarajrjpm07@gmail.com>
2025-12-20 17:09:45 +0530

    EJS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,13 +4,19 @@ const app = express()
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set EJS
+app.set("view engine", "ejs");
 
 app.post("/submit",(req,res)=>{
+    
     const name = req.body.name;
     const age = req.body.age;
     console.log("name:", name);
     console.log("age:",age);
     res.json({ message: "Data received successfully" });
+    res.render("result", { name, age });
 })
 
 
